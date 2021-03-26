dc022ee71b6d534284eda7d761df6b2bdf6c56a8
Hami Keskin <k.hami061@gmail.com>
2021-03-26 16:04:32 +0300

    commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import turtle
 
-n = 60 # number of sizes
+n = 60  # number of sizes
 
 pen = turtle.Turtle()
 
 for i in range(n):
     pen.forward(i * 10)
-    pen.right(144) # degree
+    pen.right(144)  # degree
 turtle.done()
